27a17dca0084394ca23dabba4fe1754ef1d58da9
ameycar <camilacostamejias@gmail.com>
2026-03-05 17:14:48 -0500

    app.js

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
-const firebaseConfig = { 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyDsBAnSCxSaYqP3NcbU41_2vUZAG-OcuCg",
   authDomain: "laboratorio-dr-luis-quito.firebaseapp.com",
   databaseURL: "https://laboratorio-dr-luis-quito-default-rtdb.firebaseio.com",
   projectId: "laboratorio-dr-luis-quito",
-  storageBucket: "laboratorio-dr-luis-quito.firebasestorage.app",
+  storageBucket: "laboratorio-dr-luis-quito.appspot.com",
   messagingSenderId: "165604879420",
   appId: "1:165604879420:web:659121228440c0a7f26739"
 };
 
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 const buscador = document.getElementById("buscador");
 const resultados = document.getElementById("resultados");
 
-buscador.addEventListener("input", () => {
-  const texto = buscador.value.toLowerCase();
+buscador.addEventListener("input", async () => {
 
-  database.ref("estudios").once("value", snap => {
+    const texto = buscador.value.toLowerCase();
+
+    const snapshot = await get(ref(database, "estudios"));
+
     resultados.innerHTML = "";
 
-    snap.forEach(child => {
-      const e = child.val();
+    snapshot.forEach(child => {
 
-      if (e.nombre.toLowerCase().includes(texto)) {
-        resultados.innerHTML += `
-        <div class="card">
-          <h3>${e.nombre}</h3>
-          <p class="precio">Precio: S/ ${e.precio}</p>
-          <p>Entrega: ${e.tiempo_entrega}</p>
-          <p>Ayuno: ${e.ayuno}</p>
-          <p>Preparación: ${e.preparacion}</p>
-          <p class="estado">Estado: ${e.reactivo ? "Disponible" : "Sin reactivo"}</p>
-        </div>`;
-      }
+        const e = child.val();
+
+        if(e.nombre.toLowerCase().includes(texto)){
+
+            resultados.innerHTML += `
+            <div class="card">
+              <h3>${e.nombre}</h3>
+              <p class="precio">Precio: S/ ${e.precio}</p>
+              <p>Entrega: ${e.tiempo_entrega}</p>
+              <p>Ayuno: ${e.ayuno}</p>
+              <p>Preparación: ${e.preparacion}</p>
+            </div>
+            `;
+        }
+
     });
-  });
-});
-database.ref("estudios").once("value", snap=>{
-   console.log("Conectado a Firebase", snap.val());
+
 });

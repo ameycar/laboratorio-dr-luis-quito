98c799689f30e5142505679a56217386fead58c2
ameycar <camilacostamejias@gmail.com>
2026-03-05 17:28:04 -0500

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDsBAnSCxSaYqP3NcbU41_2vUZAG-OcuCg",
   authDomain: "laboratorio-dr-luis-quito.firebaseapp.com",
   databaseURL: "https://laboratorio-dr-luis-quito-default-rtdb.firebaseio.com",
   projectId: "laboratorio-dr-luis-quito",
-  storageBucket: "laboratorio-dr-luis-quito.appspot.com",
+  storageBucket: "laboratorio-dr-luis-quito.firebasestorage.app",
   messagingSenderId: "165604879420",
   appId: "1:165604879420:web:659121228440c0a7f26739"
 };
@@ -17,31 +17,52 @@ const database = getDatabase(app);
 const buscador = document.getElementById("buscador");
 const resultados = document.getElementById("resultados");
 
-buscador.addEventListener("input", async () => {
+let estudios = [];
 
-    const texto = buscador.value.toLowerCase();
+// Cargar estudios desde Firebase
+const estudiosRef = ref(database, "estudios");
 
-    const snapshot = await get(ref(database, "estudios"));
+onValue(estudiosRef, (snapshot) => {
+
+    estudios = [];
+
+    snapshot.forEach(child => {
+        estudios.push(child.val());
+    });
+
+    mostrarEstudios(estudios);
+
+});
+
+function mostrarEstudios(lista){
 
     resultados.innerHTML = "";
 
-    snapshot.forEach(child => {
+    lista.forEach(e => {
 
-        const e = child.val();
-
-        if(e.nombre.toLowerCase().includes(texto)){
-
-            resultados.innerHTML += `
-            <div class="card">
-              <h3>${e.nombre}</h3>
-              <p class="precio">Precio: S/ ${e.precio}</p>
-              <p>Entrega: ${e.tiempo_entrega}</p>
-              <p>Ayuno: ${e.ayuno}</p>
-              <p>Preparación: ${e.preparacion}</p>
-            </div>
-            `;
-        }
+        resultados.innerHTML += `
+        <div class="card">
+            <h3>${e.nombre}</h3>
+            <p class="precio">Precio: S/ ${e.precio}</p>
+            <p>Entrega: ${e.tiempo_entrega}</p>
+            <p>Ayuno: ${e.ayuno}</p>
+            <p>Preparación: ${e.preparacion}</p>
+        </div>
+        `;
 
     });
+
+}
+
+// Buscar mientras escriben
+buscador.addEventListener("input", () => {
+
+    const texto = buscador.value.toLowerCase();
+
+    const filtrados = estudios.filter(e =>
+        e.nombre.toLowerCase().includes(texto)
+    );
+
+    mostrarEstudios(filtrados);
 
 });

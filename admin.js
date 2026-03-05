c9ae38453f982898e3d7b24de4dc0faedc5cd209
ameycar <camilacostamejias@gmail.com>
2026-03-05 18:02:30 -0500

    admin.js
    This file initializes Firebase and sets up a form submission handler to save data to the Firebase Realtime Database.

---
+++ admin.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsBAnSCxSaYqP3NcbU41_2vUZAG-OcuCg",
+  authDomain: "laboratorio-dr-luis-quito.firebaseapp.com",
+  databaseURL: "https://laboratorio-dr-luis-quito-default-rtdb.firebaseio.com",
+  projectId: "laboratorio-dr-luis-quito",
+  storageBucket: "laboratorio-dr-luis-quito.firebasestorage.app",
+  messagingSenderId: "165604879420",
+  appId: "1:165604879420:web:659121228440c0a7f26739"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit",(e)=>{
+
+e.preventDefault();
+
+const nombre = document.getElementById("nombre").value;
+const precio = document.getElementById("precio").value;
+const tiempo = document.getElementById("tiempo").value;
+const ayuno = document.getElementById("ayuno").value;
+const preparacion = document.getElementById("preparacion").value;
+const reactivo = document.getElementById("reactivo").value === "true";
+
+const id = nombre.toLowerCase().replaceAll(" ","_");
+
+set(ref(db,"estudios/"+id),{
+
+nombre:nombre,
+precio:precio,
+tiempo_entrega:tiempo,
+ayuno:ayuno,
+preparacion:preparacion,
+reactivo:reactivo
+
+});
+
+alert("Estudio guardado");
+
+form.reset();
+
+});
